b5587f4adb1f3811e39d23f431bf67270ca22c6a
Shamil Omarov <s_omarov05@mail.ru>
2020-02-21 17:39:02 +0300

    поправил ревью и добавил слайдер

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+    // ждем загрузки документов
 $(document).ready(function() {
-    var welcomeSlider = $(".welcome-slider").lightSlider({
+    // создаем переменную слайдера и активируем его с нужными настройками
+    let welcomeSlider = $(".welcome-slider").lightSlider({
         item: 1,
         pager: false,
         loop: true,
@@ -7,13 +9,14 @@ $(document).ready(function() {
         pause: 5000,
         controls: false
     }); 
-
+    // клик по кнопке стрелки назад
     $(".welcome-prev").on('click', function() {
+      // активтруем функцию, которая переключает слайдер назад
         welcomeSlider.goToPrevSlide(); 
     });
-          
+    // клик по кнопке стрелки вперед
     $(".welcome-next").on('click', function() {
+      // активтруем функцию, которая переключает слайдер вперед
         welcomeSlider.goToNextSlide(); 
     });
 });
-    

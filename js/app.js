25424243a4916dfc7a10f0e1d8d843ae096e7659
Shamil Omarov <s_omarov05@mail.ru>
2020-02-22 03:55:32 +0300

    поправил ревью секции - welcome

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
-    // ждем загрузки документов
-$(document).ready(function() {
+// ждем загрузки документов
+$(document).ready( ()=> {
+
     // создаем переменную слайдера и активируем его с нужными настройками
-    let welcomeSlider = $(".welcome-slider").lightSlider({
+    const welcomeSlider = $(".welcome-slider").lightSlider({
         item: 1,
         pager: false,
         loop: true,
         auto: true,
         pause: 5000,
         controls: false
-    }); 
-    // клик по кнопке стрелки назад
-    $(".welcome-prev").on('click', function() {
-      // активтруем функцию, которая переключает слайдер назад
-        welcomeSlider.goToPrevSlide(); 
     });
+
+    // клик по кнопке стрелки назад
+    $(".welcome-prev").on('click', ()=> {
+        // активируем функцию, которая переключает слайдер назад
+        welcomeSlider.goToPrevSlide();
+    });
+
     // клик по кнопке стрелки вперед
-    $(".welcome-next").on('click', function() {
-      // активтруем функцию, которая переключает слайдер вперед
-        welcomeSlider.goToNextSlide(); 
+    $(".welcome-next").on('click', ()=> {
+        // активируем функцию, которая переключает слайдер вперед
+        welcomeSlider.goToNextSlide();
     });
 });
